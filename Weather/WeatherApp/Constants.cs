330df8fdad8a6f3ae8668fac24a6eb89c69543ea
Sisipo Songo <ssongo@my365-uwc.ac.za>
2021-03-28 19:08:53 +0200

    Add project files.

---
+++ Weather/WeatherApp/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApp
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "1f2293305ab3806ae44a8168aef1a159";
+    }
+}
